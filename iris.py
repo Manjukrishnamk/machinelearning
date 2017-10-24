d4f101e7d2b1dc94a6e2d3d2a8393383aa79b90f
Manjukrishnamk <manjukrishnamk@cs.ajce.in>
2017-10-24 16:34:16 +0530

    first commit

--- iris.py
+++ iris.py
@@ -3,8 +3,21 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 
 iris = load_iris()
-print(iris.feature_names)
-print(iris.target_names)
-print(iris.data[0])
-print(iris.target[0])
+#print(iris.feature_names)
+#print(iris.target_names)
+#print(iris.data[0])
+#print(iris.target[0])
 
+X=[0,50,100]   # To delete the features in the iris in  0th ,50th,100
+
+xtrain = np.delete(iris.data, X, axis=0)
+ytrain =np.delete(iris.target,X)
+
+xtest = iris.data[X]
+ytest = iris.target[X]
+
+clf = DecisionTreeClassifier()
+clf.fit(xtrain,ytrain)
+
+print(ytest)
+print("prediction = ",clf.predict(xtest))
